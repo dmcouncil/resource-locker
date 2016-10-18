b946f35000431c9f1d973ca5438dbd586ac6aca1
Parker Morse <pmorse@dmcouncil.org>
2016-10-18 14:30:40 -0400

    Troubleshooting Heroku bug

--- lock.rb
+++ lock.rb
@@ -6,12 +6,12 @@ post '/' do
   return [400, ["Bad token"]] unless request['token'] == 'hYdeXb5WxvEtWPQMGyRN9cOB'
   resource_name, duration = request['text'].split unless request['text'].blank?
   if resource_name.blank?
-    return [200, {'Content-type': 'text/plain' }, [Resource.all.map(&:name).join(', ')]]
+    return [200, [Resource.all.map(&:name).join(', ')]]
   end
   # Get resource by resource_name
   resource = Resource.find_by_name(resource_name)
   if resource && duration.blank?
-    return [200, { 'Content-type': 'text/plain' }, [resource.status_string]]
+    return [200, [resource.status_string]]
   elsif resource
     # lock resource for duration
     response = {}
