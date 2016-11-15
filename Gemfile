702a698aa2fd539b1414d9e24303cb9032ea7c6e
Parker Morse <flashesofpanic@gmail.com>
2016-11-15 16:16:57 -0500

    Rspec testing (#2)
    * Rspec and unit tests for resource
    
    * Travis setup

--- Gemfile
+++ Gemfile
@@ -1,7 +1,13 @@
 source 'http://rubygems.org'
 
-gem 'sinatra'
-gem "sinatra-activerecord"
-gem "pg"
-gem "rake"
 gem 'figaro' # ENV variables
+gem 'pg'
+gem 'rake'
+gem 'sinatra'
+gem 'sinatra-activerecord'
+
+group :test do
+  gem 'capybara'
+  gem 'rspec-sinatra'
+  gem 'rspec'
+end
---
+++ spec/models/resource_spec.rb
@@ -0,0 +1,57 @@
+require 'spec_helper'
+
+describe Resource do
+  context 'when the resource is locked by someone else' do
+    let!(:resource) { Resource.new(name: 'locked-resource', locked_by: 'someone-else', locked_until: Time.now + 600) }
+
+    describe '#lock' do
+      it 'returns false' do
+        expect(resource.lock('me', 5)).to be_falsey
+      end
+    end
+
+    describe '#status_string' do
+      it 'describes the status and duration of the lock' do
+        expect(resource.status_string).to eq 'locked-resource is locked by someone-else for another 10 minutes'
+      end
+    end
+  end
+
+  context 'when the resource is locked by the user requesting the lock' do
+    let!(:resource) { Resource.new(name: 'locked-resource', locked_by: 'me', locked_until: Time.now + 600) }
+
+    describe '#lock' do
+      before do
+        resource.lock('me', 5)
+      end
+
+      it 'resets the lock to the new time' do
+        expect(resource.locked_until < (Time.now + 305)).to be_truthy
+      end
+    end
+  end
+
+  context 'when the resource is unlocked' do
+    let!(:resource) { Resource.new(name: 'locked-resource') }
+
+    describe '#lock' do
+      before do
+        resource.lock('me', 5)
+      end
+
+      it 'sets the lock 5 minutes out' do
+        expect(resource.locked_until < (Time.now + 300)).to be_truthy 
+      end
+
+      it 'is locked by the user requesting the lock' do
+        expect(resource.locked_by).to eq 'me'
+      end
+    end
+
+    describe '#status_string' do
+      it 'says the resource is not locked' do
+        expect(resource.status_string).to eq 'locked-resource is not locked'
+      end
+    end
+  end
+end
